0f1e8209f55dc662291c5835ca55ad67725d8b95
Daniel Frystacky <danielfrystacky@gmail.com>
2018-11-25 13:31:43 -0500

    Audio for block breaks and ball bounce

--- Unity-Block-Breaker/Assets/Scripts/Ball.cs
+++ Unity-Block-Breaker/Assets/Scripts/Ball.cs
@@ -9,14 +9,19 @@ public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
   
     //state
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
+    //cached componets references
+    AudioSource myAudioSource;
+
     // Use this for initialization
     void Start () {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
 
 	}
 	
@@ -45,4 +50,15 @@ public class Ball : MonoBehaviour {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+        
+    }
+
 }
--- Unity-Block-Breaker/Assets/Scripts/Block.cs
+++ Unity-Block-Breaker/Assets/Scripts/Block.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    //config params
+    [SerializeField] AudioClip breakSound;
+
+    //cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
